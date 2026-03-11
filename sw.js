b9e9b01db4fd9e74f68254d57e3309093c680b91
MitoPulse Admin <admin@mitopulse.com>
2026-03-11 14:44:30 -0300

    Add manifest and service worker registration for PWA installability

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mitopulse-v30-cache-v7';
+const CACHE_NAME = 'mitopulse-v30-cache-v8';
 const urlsToCache = [
     './index.html',
     'https://cdn.jsdelivr.net/npm/tweetnacl@1.0.3/nacl.min.js',
