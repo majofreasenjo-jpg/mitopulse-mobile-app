1d1d6d4ca9003b0b51a1822a9abf91d20d46c296
MitoPulse Admin <admin@mitopulse.com>
2026-03-11 01:39:51 -0300

    Initial commit of PWA UI

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'mitopulse-v30-cache-v3';
+const urlsToCache = [
+    './index.html',
+    'https://cdn.jsdelivr.net/npm/tweetnacl@1.0.3/nacl.min.js',
+    'https://cdn.jsdelivr.net/npm/tweetnacl-util@0.15.1/nacl-util.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
